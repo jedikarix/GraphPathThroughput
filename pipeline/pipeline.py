52da95f53b465bf5d57268186306fe06982213e2
jedikarix <karolchecinski@wp.pl>
2019-05-10 20:53:53 +0200

    Implement graphs loading functions

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -2,15 +2,28 @@ from typing import List, Union, Tuple
 import networkx as nx
 
 
-def load_graphs_list(filename: str) -> List[nx.Graph]:
-    return list()
+def load_graphs_from_file(filename: str) -> List[nx.Graph]:
+    with open(filename) as file:
+        graphs_filenames = file.readlines()
+    return [nx.read_edgelist(graph_filename) for graph_filename in graphs_filenames]
 
 
 def load_graphs(graphs: List[Union[str, nx.Graph]]) -> List[nx.Graph]:
-    return list()
+    loaded_graphs = list()
+    for graph in graphs:
+        if isinstance(graph, str):
+            graph = nx.read_edgelist(graph)
+        loaded_graphs.append(graph)
+    return loaded_graphs
 
 
-def pipeline(graphs: List[Union[str, nx.Graph]]) -> None:
+def pipeline(graphs_list: Union[List[Union[str, nx.Graph]], str] = None) -> None:
+    if isinstance(graphs_list, str):
+        graphs = load_graphs_from_file(graphs_list)
+    elif isinstance(graphs_list, List):
+        graphs = load_graphs(graphs_list)
+    else:
+        raise Exception("graphs_list should be filename or list")
     graphs = load_graphs(graphs)
     results = run_algorithm(graphs)
     process_results(results)

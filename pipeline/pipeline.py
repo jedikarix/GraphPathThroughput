488811cba53ee86ec45baa056a7a471926697622
jedikarix <karolchecinski@wp.pl>
2019-05-10 21:31:31 +0200

    Implement run_algorithm function

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,4 +1,6 @@
-from typing import List, Union, Tuple
+import time
+from typing import List, Union, Tuple, Generator, Callable, Any
+
 import networkx as nx
 
 
@@ -17,21 +19,30 @@ def load_graphs(graphs: List[Union[str, nx.Graph]]) -> List[nx.Graph]:
     return loaded_graphs
 
 
-def pipeline(graphs_list: Union[List[Union[str, nx.Graph]], str] = None) -> None:
+def pipeline(graphs_list: Union[Generator, List[Union[str, nx.Graph]], str],
+             algorithm: Callable[[nx.Graph], Any]) -> None:
     if isinstance(graphs_list, str):
         graphs = load_graphs_from_file(graphs_list)
     elif isinstance(graphs_list, List):
         graphs = load_graphs(graphs_list)
+    elif isinstance(graphs_list, Generator):
+        graphs = graphs_list
     else:
         raise Exception("graphs_list should be filename or list")
-    graphs = load_graphs(graphs)
-    results = run_algorithm(graphs)
+    results = run_algorithm(graphs, algorithm)
     process_results(results)
     pass
 
 
-def run_algorithm(graphs: List[nx.Graph]) -> List[Tuple[float, List[int], int]]:
-    return list()
+def run_algorithm(graphs: Union[Generator, List[nx.Graph]],
+                  algorithm: Callable[[nx.Graph], Any]) -> List[Tuple[float, Any]]:
+    results = list()
+    for graph in graphs:
+        t0 = time.clock()
+        alg_result = algorithm(graph)
+        dt = time.clock() - t0
+        results.append((dt, alg_result))
+    return results
 
 
 def process_results(results):

329fb711213909bf7e20bbca246ee0a872e8b3f6
jedikarix <karolchecinski@wp.pl>
2019-05-11 21:32:12 +0200

    Move build_dfs_tree and low functions to utils.py

--- algorithms/segment_graph.py
+++ algorithms/segment_graph.py
@@ -1,50 +1,14 @@
-from typing import Dict, Tuple
-
 import networkx as nx
 
-
-def build_dfs_tree(G: nx.Graph, root=None) -> nx.DiGraph:
-    if root is None:
-        root = list(G.nodes())[0]
-    depth = dict()
-    T = nx.DiGraph()
-    track = [root]
-    T.add_node(root)
-    depth[root] = 0
-    while len(track) != 0:
-        curr = track[-1]
-        child_candidates = [v for v in G.neighbors(curr) if not T.has_node(v)]
-        if len(child_candidates) == 0:
-            track.pop()
-        else:
-            child = child_candidates[0]
-            track.append(child)
-            T.add_node(child)
-            T.add_edge(curr, child)
-            depth[child] = depth[curr] + 1
-    nx.set_node_attributes(T, depth, "depth")
-    return T
-
-
-def low(G: nx.Graph, root) -> Tuple[Dict[int, int], nx.Graph]:
-    low_map = dict()
-
-    T = build_dfs_tree(G, root)
-    depth = nx.get_node_attributes(T, "depth")
-    T_nodes = sorted(list(T.nodes()), reverse=True, key=lambda v: depth[v])
-    T_ud = T.to_undirected()
-    G_secondary = nx.difference(G, T_ud)
-
-    for v in T_nodes:
-        v_children_low = [low_map[w] for w in T.neighbors(v)]
-        v_sec_dep = [depth[w] for w in G_secondary.neighbors(v)]
-        v_dep = depth[v]
-        low_map[v] = min(v_children_low + v_sec_dep + [v_dep])
-
-    return low_map, T
+from algorithms.utils import low
 
 
 def segment_graph(G: nx.Graph) -> nx.Graph:
+    """
+    Generate graph
+    :param G:
+    :return:
+    """
     segments = list([set()])
     art_points = set()
     curr_seg = 0
---
+++ algorithms/utils.py
@@ -0,0 +1,56 @@
+from typing import Dict, Tuple
+
+import networkx as nx
+
+
+def build_dfs_tree(G: nx.Graph, root=None) -> nx.DiGraph:
+    """
+    Build DFS tree based on given graph. Add to result graph's nodes attribute with node's depth in tree.
+    :param G: Graph for building tree.
+    :param root: root node. If unspecified, algorithm take first node from nodes list as root.
+    :return: Oriented tree with depth assigned to nodes.
+    """
+    if root is None:
+        root = list(G.nodes())[0]
+    depth = dict()
+    T = nx.DiGraph()
+    track = [root]
+    T.add_node(root)
+    depth[root] = 0
+    while len(track) != 0:
+        curr = track[-1]
+        child_candidates = [v for v in G.neighbors(curr) if not T.has_node(v)]
+        if len(child_candidates) == 0:
+            track.pop()
+        else:
+            child = child_candidates[0]
+            track.append(child)
+            T.add_node(child)
+            T.add_edge(curr, child)
+            depth[child] = depth[curr] + 1
+    nx.set_node_attributes(T, depth, "depth")
+    return T
+
+
+def low(G: nx.Graph, root) -> Tuple[Dict[int, int], nx.Graph]:
+    """
+    Calculate low function values for graph nodes.
+    :param G: input Graph
+    :param root: root node
+    :return: tuple (low_map, tree) with dictionary mapping nodes to low function values and tree used in computations
+    """
+    low_map = dict()
+
+    T = build_dfs_tree(G, root)
+    depth = nx.get_node_attributes(T, "depth")
+    T_nodes = sorted(list(T.nodes()), reverse=True, key=lambda v: depth[v])
+    T_ud = T.to_undirected()
+    G_secondary = nx.difference(G, T_ud)
+
+    for v in T_nodes:
+        v_children_low = [low_map[w] for w in T.neighbors(v)]
+        v_sec_dep = [depth[w] for w in G_secondary.neighbors(v)]
+        v_dep = depth[v]
+        low_map[v] = min(v_children_low + v_sec_dep + [v_dep])
+
+    return low_map, T

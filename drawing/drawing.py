33d5934d4f3f710e5f20ec27cfbe8e0a0039e218
jedikarix <karolchecinski@wp.pl>
2019-05-14 15:48:44 +0200

    Add draw_available_subgraph function

--- drawing/drawing.py
+++ drawing/drawing.py
@@ -1,6 +1,7 @@
 import networkx as nx
 
 from algorithms.utils import path_from_list
+from algorithms.available_subgraph import available_subgraph
 
 
 def draw_weighted_graph(G: nx.Graph, edge_attr='throughput', path=None, pos=None):
@@ -21,3 +22,17 @@ def draw_segment_graph(G: nx.Graph, segment_color='red', art_color='green'):
     for node in G.nodes():
         colors.append(art_color if articulation[node] else segment_color)
     nx.draw(G, with_labels=True, node_color=colors)
+
+
+def draw_available_subgraph(G: nx.Graph, s: int, t: int, remove_rest: bool = False, pos=None):
+    if pos is None:
+        pos = nx.spring_layout(G)
+    H = available_subgraph(G, s, t)
+    if remove_rest:
+        draw_weighted_graph(H, pos)
+    else:
+        nodes_colors = ['red' if H.has_node(v) else 'green' for v in G.nodes()]
+        edges_color = ['red' if H.has_edge(e[0], e[1]) else 'black' for e in G.edges()]
+        nx.draw(G, pos=pos, with_labels=True, edge_color=edges_color, node_color=nodes_colors)
+        nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=nx.get_edge_attributes(G, 'throughput'))
+    return pos

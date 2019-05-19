bc8b5ccba9747dc966795244f4ebe72933432391
Piotr Komorowski <pjpkomorowski@gmail.com>
2019-05-19 23:55:59 +0200

    List of source and targets

--- pipeline/run_tests.py
+++ pipeline/run_tests.py
@@ -8,12 +8,12 @@ import matplotlib.pyplot as plt
 
 def run_unit_tests(dir):
 
-    s_t = [(1, 2), (1, 2), (1, 2), (1, 2), (1, 2),
-           (1, 2), (1, 2), (1, 2), (1, 2), (1, 2),
-           (1, 2), (1, 2), (1, 2), (1, 2), (1, 2),
-           (1, 2), (1, 2), (1, 2), (1, 2), (1, 2),
-           (1, 2), (1, 2), (1, 2), (1, 2), (1, 2),
-           (1, 2), (1, 2)]
+    s_t = [(1, 2), (1, 3), (1, 4), (1, 3), (1, 4),
+           (1, 4), (2, 6), (1, 4), (4, 6), (1, 7),
+           (2, 6), (1, 6), (3, 7), (1, 5), (2, 7),
+           (1, 4), (1, 6), (1, 7), (3, 10), (1, 10),
+           (5, 12), (1, 6), (2, 7), (1, 12), (1, 20),
+           (11, 19), (1, 10)]
 
     files = [os.path.join(dir, filename) for filename in sorted(os.listdir(dir))]
     graphs = load_graphs(files)
@@ -32,4 +32,3 @@ def run_unit_tests(dir):
         draw_weighted_graph(graph, edge_attr="weight")
         plt.savefig("plot_{}.png".format(i), edge_attr="weight")
         plt.clf()
-

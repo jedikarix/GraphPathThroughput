75728f6128023531b0f94c4b5f7312916c4a558a
jedikarix <karolchecinski@wp.pl>
2019-05-20 20:05:57 +0200

    Remove remove_null parameter from algorithms

--- algorithms/min_throughput_path.py
+++ algorithms/min_throughput_path.py
@@ -6,7 +6,7 @@ from algorithms.available_subgraph import available_subgraph
 from algorithms.utils import filter_null_edges, path_with_edge
 
 
-def min_throughput_path(G: nx.Graph, s: int, t: int, remove_null:bool = True, attr_name="throughput") -> Tuple[List[int], int]:
+def min_throughput_path(G: nx.Graph, s: int, t: int, attr_name="throughput") -> Tuple[List[int], int]:
     """
     Finds path between s and t nodes with minimal throughput
     :param G: input graph
@@ -14,9 +14,6 @@ def min_throughput_path(G: nx.Graph, s: int, t: int, remove_null:bool = True, at
     :param t: target node
     :return: tuple (path, throughput)
     """
-
-    if remove_null:
-        G = filter_null_edges(G)
 
     if not nx.has_path(G, s, t):
         raise Exception("There is not path between s and t")
